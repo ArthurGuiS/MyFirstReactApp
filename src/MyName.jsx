0692d202f78bef796f57ff8a81a57fdf02b0c861
ArthurGuiS <arthur12304@hotmail.com>
2023-11-08 11:21:10 -0300

    att

--- src/MyName.jsx
+++ src/MyName.jsx
@@ -1,23 +1,27 @@
 import { useState } from "react"
 
 function MyName(){
+    const [text,setText] = useState("")
     const [name, setName] = useState("")
     
 
     const changeName = (e) => {
         setName(e.target.value)
-        
+    }
+
+    function changeText() {
+        setText(name)
     }
 
     return (
         <>
         <div>
             <label for="name">Name:</label>
-            <input type="text" id="name" onChange={changeName}/>
+            <input type="text" value={name} onChange={changeName}/>
+            <button onClick={changeText}>Hello</button>
         </div>
         <div>
-            <p>Hello, {name}</p>
-
+            <p>Hello, {text}</p>
         </div>
         </>
     )
